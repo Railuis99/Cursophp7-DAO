ea0156a146f8f443ec2501069f4efc7a186b1fd6
Rai Luis <rai.luis1999@gmail.com>
2021-09-17 19:46:47 -0300

    Metodos de lista

--- index.php
+++ index.php
@@ -8,11 +8,32 @@ require_once("config.php");
 
 //echo json_encode($usuarios);
 
+//carrega um usuario
+//$root = new Usuario();
 
-$root = new Usuario();
+//$root->loadById(3);
 
-$root->loadById(3);
+//echo $root;
 
-echo $root;
+
+//carrega uma lista de usuarios
+//pelo fato do metodo não usar nenhum atributo da classe usuario, ele se torna um metodo estatico e pode ser chamado diretamente sem ser instaciado
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
+
+//carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("jo");
+
+//echo json_encode($search);
+
+
+//carrega um usuario usando o login e senha
+$usuario = new Usuario ();
+$usuario->login("root", "!@#$");
+
+echo $usuario;
+
+
 
 ?>
